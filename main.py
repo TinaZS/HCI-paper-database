2c33c52b534c284fb78b4f5b35826019497ba820
SebastienBrown <107078090+SebastienBrown@users.noreply.github.com>
2025-02-17 19:49:46 +0000

    Automate upload of updated FAISS_index.index file to Supabase

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from src.rebuild_faiss import needs_rebuild, rebuild_faiss
 from src.config import FAISS_INDEX_FILENAME
 from src.size_test import file_size_test
 from src.scrape_CS import get_category_size
+from src.FAISS_upload import replace_file
 import time
 import json
 
@@ -29,9 +30,9 @@ def main():
     pullSize=100 #this ensures supabase query does not exceed limit 
     for key in category_dict:
         maxPulls=category_dict[key]
-        counter=1000
+        counter=2000
 
-        while(counter<1500):
+        while(counter<2500):
             
             #Fetch papers from arXiv
             xml_data = fetch_arxiv_data(key,pullSize,counter)
@@ -48,24 +49,9 @@ def main():
             generate_and_store_embeddings(unique_papers)
             
             counter+=pullSize
-
-    #Load FAISS index and run search
-    index = load_index(FAISS_INDEX_FILENAME)
     
-    index=load_index("faiss_index.index")
-    #num_embeddings = index.ntotal
-    #print("FAISS contains",num_embeddings,"embeddings")
-    
-    if index:
-        query = "How do I collect data in a fair way"  # User query
-        results = search(query, index)
+    replace_file("faiss-index", "faiss_index.index", "faiss_index.index")
 
-        for result in results:
-            print(f"Title: {result['title']}\n  {result['link']}\n")
-    else:
-        print("ERROR: result array is empty")
-  
-    #file_size_test()
     
 if __name__ == "__main__":
     main()
